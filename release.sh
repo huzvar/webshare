673cfc582e4718c8a96d3ec947dbc04564f59728
Robert Huzvar <huzvar@gmail.com>
2025-05-17 23:36:15 +0200

    repository

--- release.sh
+++ release.sh
@@ -3,3 +3,7 @@
 zip -r docs/repository.webshare.zip repository.webshare
 
 # shasum -a 1 docs/repository.webshare.zip > docs/repository.webshare.zip.md5
+
+cp repository/addons.xml docs/addons.xml
+
+md5sum repository/addons.xml | cut -d ' ' -f 1 > docs/addons.xml.md5
