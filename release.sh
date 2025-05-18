7433067b691efc562c073d445a52cb5ba2fb81a1
Robert Huzvar <huzvar@gmail.com>
2025-05-18 12:24:42 +0200

    clean

--- release.sh
+++ release.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-rm docs/repository.webshare.zip
-zip -r docs/repository.webshare.zip repository.webshare
-
-# shasum -a 1 docs/repository.webshare.zip > docs/repository.webshare.zip.md5
+# rm docs/repository.webshare.zip
+# zip -r docs/repository.webshare.zip repository.webshare
 
 cd repository
 
-cp addons.xml ../docs/addons.xml
-md5sum addons.xml > ../docs/addons.xml.md5
+# cp addons.xml ../docs/addons.xml
+# md5sum addons.xml > ../docs/addons.xml.md5
 
-VERSION=`awk -F'version="' '/<addon/{split($2,a,"\""); print a[1]}' plugin.video.webshare/addon.xml`
-rm ../docs/plugin.video.webshare/plugin.video.webshare-${VERSION}.zip
-zip -r ../docs/plugin.video.webshare/plugin.video.webshare-${VERSION}.zip plugin.video.webshare
+# VERSION=`awk -F'version="' '/<addon/{split($2,a,"\""); print a[1]}' plugin.video.webshare/addon.xml`
+# rm ../docs/plugin.video.webshare/plugin.video.webshare-${VERSION}.zip
+# zip -r ../docs/plugin.video.webshare/plugin.video.webshare-${VERSION}.zip plugin.video.webshare
+
+
+zip -r ../docs/plugin.video.webshare.zip plugin.video.webshare
