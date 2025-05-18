0ccc57fd92ffbf56164eeee4b7c6e3925063cb6e
Robert Huzvar <huzvar@gmail.com>
2025-05-18 11:53:47 +0200

    v2

--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ zip -r docs/repository.webshare.zip repository.webshare
 cd repository
 
 cp addons.xml ../docs/addons.xml
-md5sum addons.xml | cut -d ' ' -f 1 > ../docs/addons.xml.md5
+md5sum addons.xml > ../docs/addons.xml.md5
 
 VERSION=`awk -F'version="' '/<addon/{split($2,a,"\""); print a[1]}' plugin.video.webshare/addon.xml`
 rm ../docs/plugin.video.webshare/plugin.video.webshare-${VERSION}.zip
